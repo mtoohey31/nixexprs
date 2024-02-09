57562f532a6001b85083309feed477ddcd1e824d
Matthew Toohey <contact@mtoohey.com>
2024-02-08 21:34:40 -0500

    remove unused usercustomize files

--- home-manager/modules/mtoohey/common/usercustomize3.py
+++
@@ -1,9 +0,0 @@
-try:
-    sys
-    sys.ps1 = 'py \x1b[33m>\x1b[0m '
-    sys.ps2 = '     '
-except NameError:
-    import sys
-    sys.ps1 = 'py \x1b[33m>\x1b[0m '
-    sys.ps2 = '     '
-    del sys
