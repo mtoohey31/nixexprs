7106f3f0f17b4dd8b7602a5a821b84d81df6e797
Matthew Toohey <contact@mtoohey.com>
2024-02-27 16:53:25 -0500

    kitty fallback fonts workaround

--- pkgs/applications/terminal-emulators/kitty/default.nix
+++ pkgs/applications/terminal-emulators/kitty/default.nix
@@ -2,8 +2,9 @@
 
 kitty.overrideAttrs (oldAttrs: {
   patches = (oldAttrs.patches or [ ]) ++ [
-    ./0001-Revert-Use-actual-color-value-comparison-when-detect.patch
     ./0001-Enable-E3-terminfo-capability-to-properly-clear-scro.patch
+    ./0001-Increase-fallback-fonts-limit.patch
+    ./0001-Revert-Use-actual-color-value-comparison-when-detect.patch
   ];
   # regenerate terminfo stuff based on the patch; we could include the updated
   # generated files in the patch, but then the patch won't apply as soon as any
