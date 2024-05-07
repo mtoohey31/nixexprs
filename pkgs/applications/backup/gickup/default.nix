95061a59da739f2f0b2318e5e094b9c79c0274d0
Matthew Toohey <contact@mtoohey.com>
2024-05-07 14:17:52 -0400

    update inputs

--- pkgs/applications/backup/gickup/default.nix
+++ pkgs/applications/backup/gickup/default.nix
@@ -2,12 +2,12 @@
 
 buildGoModule rec {
   pname = "gickup";
-  version = "0.10.28";
+  version = "0.10.30";
   src = fetchFromGitHub {
     owner = "cooperspencer";
     repo = pname;
     rev = "v${version}";
-    sha256 = "IGzwMSpbGiUjlO7AtxL20m72VXYW3MJemLpO5BN2rMo=";
+    sha256 = "knnc4FAzGk1hV/Pzoc+klm4dt1cFrn4BYZx1lY7iLp8=";
   };
-  vendorHash = "sha256-sINmTwUERhxZ/qEAhKiJratWV6fDxrP21cJg97RBKVc=";
+  vendorHash = "sha256-XxDsEmi945CduurQRsH7rjFAEu/SMX3rSd63Dwq2r8A=";
 }
