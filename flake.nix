fa5ea2529f21974dd14297d2e77ac7d6242d23e2
Matthew Toohey <contact@mtoohey.com>
2024-12-26 14:52:18 -0500

    switch to pre-commit-hooks

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -74,37 +78,67 @@
     };
   };
 
-  outputs = { self, nixpkgs, utils, nix-index-database, ... }@inputs: {
-    lib = import ./lib nixpkgs.lib;
-
-    darwinModules = self.lib.callModules {
-      moduleListPath = ./nix-darwin/modules/module-list.nix;
-      inherit inputs;
-      selfName = "nixexprs";
-    };
-    homeManagerModules = {
-      inherit (nix-index-database.hmModules) nix-index;
-    } // self.lib.callModules {
-      moduleListPath = ./home-manager/modules/module-list.nix;
-      inherit inputs;
-      selfName = "nixexprs";
-    };
-
-    overlays.default = _: prev: let pkgs = prev; in import ./pkgs inputs pkgs;
-  } // utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        overlays = builtins.attrValues self.overlays;
-        inherit system;
-      };
-      inherit (pkgs) deadnix gnumake mkShell nil nixpkgs-fmt statix;
-    in
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    , nix-index-database
+    , pre-commit-hooks
+    , ...
+    }@inputs:
     {
-      devShells.default = mkShell {
-        packages = [ deadnix gnumake nil nixpkgs-fmt statix ];
+      lib = import ./lib nixpkgs.lib // {
+        pre-commit-hooks = {
+          deadnix.enable = true;
+          nil.enable = true;
+          nixpkgs-fmt = {
+            enable = true;
+            args = [ "--check" ];
+          };
+          statix = {
+            enable = true;
+            settings.config = ".statix.toml";
+          };
+        };
       };
 
-      packages = self.lib.filterSupportedPackages system
-        (import ./pkgs inputs (import nixpkgs { inherit system; }));
-    });
+      darwinModules = self.lib.callModules {
+        moduleListPath = ./nix-darwin/modules/module-list.nix;
+        inherit inputs;
+        selfName = "nixexprs";
+      };
+      homeManagerModules = {
+        inherit (nix-index-database.hmModules) nix-index;
+      } // self.lib.callModules {
+        moduleListPath = ./home-manager/modules/module-list.nix;
+        inherit inputs;
+        selfName = "nixexprs";
+      };
+
+      overlays.default = _: prev: let pkgs = prev; in import ./pkgs inputs pkgs;
+    } // utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          overlays = builtins.attrValues self.overlays;
+          inherit system;
+        };
+        inherit (pkgs) gnumake mkShell nil;
+      in
+      rec {
+        checks.pre-commit = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = self.lib.pre-commit-hooks;
+        };
+
+        devShells.default = mkShell {
+          inherit (checks.pre-commit) shellHook;
+          packages = [ gnumake nil ];
+        };
+
+        packages = self.lib.filterSupportedPackages system (
+          import ./pkgs inputs (import nixpkgs { inherit system; })
+        );
+      }
+    );
 }
